50c44a5b4b7d63eba66af9cea44240830718a319
seeand <kdsfoxy@gmail.com>
2020-03-02 23:17:56 -0600

    Section 5 - 50.InternalAccessModifier

---
+++ Section-5/OO-Kotlin-Classes-Functions-Inheritance/src/com/mx/kotlin/_50/InternalAccessModifier/InternalAccesModifier.kt
@@ -0,0 +1,8 @@
+package com.mx.kotlin._50.InternalAccessModifier
+
+fun main(args: Array<String>) {
+
+}
+
+//makes private this function
+internal fun someFun() : String = "This an internal function"
